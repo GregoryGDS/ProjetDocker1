fc09bda6aa8d933a35e218b8b7db9775c460d63f
gregory <gregory.dossantos98@gmail.com>
2019-10-16 23:00:26 +0200

    màj mail

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,6 @@
 FROM php:7.2-apache
 
 RUN docker-php-ext-install pdo_mysql
-
 RUN apt-get update &&\
     apt-get install --no-install-recommends --assume-yes --quiet ca-certificates curl git &&\
     rm -rf /var/lib/apt/lists/*
--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php
-echo "Faite que ça marche !!! ;-;";
+echo "Faite que ça marche !!! ;-; <br>";
+echo "Docker ne m'aime pas <br>";
+mail("hamen@outlook.fr", "ProjetDocker1", "Prions pour que ça marche .... ", "From: ripDocker@gmail.com");
 ?>
 <br>
 <img src="https://i.giphy.com/media/d2lcHJTG5Tscg/giphy.webp">
+
+
